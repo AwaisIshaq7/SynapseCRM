74a997240cf6d2f3d714a9733d097952d69694a4
AwaisIshaq7 <149109306+AwaisIshaq7@users.noreply.github.com>
2026-05-13 15:55:47 +0500

    Import deleteInteraction from interactionController

--- backend/src/routes/interactionRoutes.js
+++ backend/src/routes/interactionRoutes.js
@@ -1,3 +1,10 @@
+const {
+  getInteractions,
+  createInteraction,
+} = require('../controllers/interactionController');
+
+const { deleteInteraction } = require('../controllers/interactionController');
+
 const express = require('express');
 const router = express.Router({ mergeParams: true }); // mergeParams gives access to :id from parent
 const {
