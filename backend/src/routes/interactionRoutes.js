20083cd54f530f2687705c8fe78a2398240bb4bc
AwaisIshaq7 <149109306+AwaisIshaq7@users.noreply.github.com>
2026-05-13 16:20:41 +0500

    Refactor interactionRoutes to consolidate imports

--- backend/src/routes/interactionRoutes.js
+++ backend/src/routes/interactionRoutes.js
@@ -1,22 +1,16 @@
-const {
-  getInteractions,
-  createInteraction,
-} = require('../controllers/interactionController');
-
-const { deleteInteraction } = require('../controllers/interactionController');
-
 const express = require('express');
-const router = express.Router({ mergeParams: true }); // mergeParams gives access to :id from parent
+const router = express.Router({ mergeParams: true });
 const {
   getInteractions,
   createInteraction,
+  deleteInteraction,
 } = require('../controllers/interactionController');
-const { deleteInteraction } = require('../controllers/interactionController');
 const { protect } = require('../middleware/auth');
 
 router.use(protect);
 
 router.get('/', getInteractions);
 router.post('/', createInteraction);
+router.delete('/:interactionId', deleteInteraction);
 
 module.exports = router;
